a702f480ae9a79c583fb8223398dde2c0ecaa9ba
Cem HOROZOGLU <cem.horozoglu@epitech.eu>
2023-04-12 12:20:22 +0200

    Ex 18 : check "permission" in array, if in array $permission

--- day_03/PoolDay5/step_18/ex_18.php
+++ day_03/PoolDay5/step_18/ex_18.php
@@ -3,7 +3,7 @@
 function check_user_acl($user, $permission){
     foreach($user as $key => $values){
         if($key === "permissions"){
-            if(is_array($values) && in_array($permission, $values) || $values === $permission)
+            if((is_array($values) && in_array($permission, $values)) || $values === $permission)
                 return true;
         }
     }
