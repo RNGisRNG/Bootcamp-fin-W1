c896a6e36675ba974552a7a17907aadf88efaadd
Cem HOROZOGLU <cem.horozoglu@epitech.eu>
2023-04-28 10:20:49 +0200

    ex 01

---
+++ day_13/codingame/JS/step_01/ex_01.js
@@ -0,0 +1,19 @@
+window.addEventListener(("load"), ()=>{
+    display_recu(5, "_");
+});
+
+function display_recu(nbrLoop, symbol){
+    let str = "";
+    let space = "";
+
+    for(let i = 0; i < nbrLoop; i++){
+        if(i === nbrLoop-1){
+            str += space+symbol;
+        }else{
+            str += space+symbol+"\n";
+            space += " ";
+        }
+    }
+
+    console.log(str);
+}
